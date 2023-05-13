b36a9b824a1ba772238027d36ca438e81e507520
Sofenos <soufianainos@gmail.com>
2023-05-13 16:38:58 -0700

    My Work

---
+++ find_bin_path.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * find_bin_path -
+ *
+ * Return: NULL
+ */
+
+
+char* find_bin_path(char* bin_name) {
+    char* path = getenv("PATH");
+    char* token = strtok(path, ":");
+    struct stat sb;
+
+    while (token != NULL) {
+        char bin_path[BUFFER_SIZE];
+        snprintf(bin_path, BUFFER_SIZE, "%s/%s", token, bin_name);
+
+        if (stat(bin_path, &sb) == 0 && sb.st_mode & S_IXUSR)
+            return strdup(bin_path);
+
+        token = strtok(NULL, ":");
+    }
+
+    return (NULL);
+}
---
+++ parse_command.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * parse_command -
+ *
+ * Return: Void
+ */
+
+void parse_command(char* command, char** args) {
+    char* token = strtok(command, " ");
+    int i = 0;
+    while (token != NULL) {
+        args[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    args[i] = NULL;
+}
