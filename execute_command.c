46c87ead059aa192ff8576f91cb72fa4157cbcaa
Sofenos <soufianainos@gmail.com>
2023-05-22 14:20:27 -0700

    Fix Betty style

--- execute_command.c
+++ execute_command.c
@@ -8,17 +8,17 @@
  */
 void cd_command(char **args)
 {
-if (args[1] == NULL)
-{
-chdir(getenv("HOME"));
-}
-else
-{
-if (chdir(args[1]) != 0)
-{
-printf("Error navigating to specified directory\n");
-}
-}
+	if (args[1] == NULL)
+	{
+		chdir(getenv("HOME"));
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			printf("Error navigating to specified directory\n");
+		}
+	}
 }
 
 /**
@@ -28,7 +28,7 @@ printf("Error navigating to specified directory\n");
  */
 void exit_command(void)
 {
-exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
 /**
@@ -39,11 +39,11 @@ exit(EXIT_SUCCESS);
 void env_command(void)
 {
 char **env_var = environ;
-while (*env_var != NULL)
-{
-printf("%s\n", *env_var);
-env_var++;
-}
+	while (*env_var != NULL)
+	{
+		printf("%s\n", *env_var);
+		env_var++;
+	}
 }
 
 /**
@@ -54,35 +54,36 @@ env_var++;
  */
 void execute_command(char **args)
 {
-if (strcmp(args[0], "cd") == 0)
-{
-cd_command(args);
+	if (strcmp(args[0], "cd") == 0)
+	{
+		cd_command(args);
+	}
+	else if (strcmp(args[0], "exit") == 0)
+	{
+		exit_command();
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		env_command();
+	}
+	else
+	{
+	pid_t pid = fork();
+		if (pid == 0)
+		{
+			execvp(args[0], args);
+			fprintf(stderr, "%s: Command not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			fprintf(stderr, "Fork error\n");
+		}
+		else
+		{
+		int status;
+			waitpid(pid, &status, 0);
+		}
+	}
 }
-else if (strcmp(args[0], "exit") == 0)
-{
-exit_command();
-}
-else if (strcmp(args[0], "env") == 0)
-{
-env_command();
-}
-else
-{
-pid_t pid = fork();
-if (pid == 0)
-{
-execvp(args[0], args);
-fprintf(stderr, "%s: Command not found\n", args[0]);
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-{
-fprintf(stderr, "Fork error\n");
-}
-else
-{
-int status;
-waitpid(pid, &status, 0);
-}
-}
-}
+
